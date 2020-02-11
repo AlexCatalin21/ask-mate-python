555dd49c90f9d4c7fb42f8159f5fd6bda532bcf2
904756@gmail.com <904756@gmail.com>
2020-02-11 14:43:41 +0200

    Latest v

--- data_manager.py
+++ data_manager.py
@@ -4,8 +4,8 @@ def read_from_file(filename):
     questions=[]
     with open(filename,"r") as file:
         for row in file:
-            questions.append([row.strip("\n").split(',')])
+            questions.append(row.strip("\n").split(','))
     return questions
 
 
-print(read_from_file("question.csv"))
+print(read_from_file("sample_data/question.csv"))
--- server.py
+++ server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/list')
 def main_page():
-    return render_template('index.html', table_elements = data_manager.read_from_file('question.csv'))
+    return render_template('index.html', table_elements = data_manager.read_from_file('sample_data/question.csv'))
 
 
 if __name__ == '__main__':
