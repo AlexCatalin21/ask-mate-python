94172566f3ab9c4a70546cbd20e34d0f51ad6201
AlexCatalin21 <alexandru.g2101@gmail.com>
2020-02-11 11:19:52 +0200

    data_manager added

---
+++ data_manager.py
@@ -0,0 +1,11 @@
+import csv
+
+def read_from_file(filename):
+    questions=[]
+    with open(filename,"r") as file:
+        for row in file:
+            questions.append([row.strip("\n")])
+    return questions
+
+
+print(read_from_file("sample_data/question.csv"))
