9f307049530ca2e4c5eeecfd70ab6bdb7df195c2
AlexCatalin21 <alexandru.g2101@gmail.com>
2020-02-11 21:14:06 +0200

    table done

---
+++ server.py
@@ -0,0 +1,16 @@
+import data_manager
+
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/list')
+def main_page():
+    return render_template('index.html', table_elements = data_manager.read_from_file('sample_data/question.csv'))
+
+
+if __name__ == '__main__':
+    app.run(
+        debug=True
+    )
