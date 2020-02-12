c9f953035063bd4c7f76d1dbd1cb4c1a8a46ba05
MihaiBarbu93 <mihai.barbu1993@gmail.com>
2020-02-12 15:22:42 +0200

    question user story done and answer user story done

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import data_manager
+import data_manager, sorting_functions
 
 from flask import Flask, render_template, request, redirect, url_for
 
@@ -9,6 +9,11 @@ app = Flask(__name__)
 def main_page():
     return render_template('index.html', table_elements = data_manager.read_from_file('sample_data/question.csv'))
 
+@app.route('/question/<question_id>', methods=["GET"])
+def show_questions(question_id):
+
+
+    return render_template('question.html', question_elements= sorting_functions.title_and_message(question_id), answer_elements= sorting_functions.get_answer(question_id), question_id=question_id )
 
 if __name__ == '__main__':
     app.run(
---
+++ sorting_functions.py
@@ -0,0 +1,24 @@
+import data_manager
+def title_and_message(id):
+    list_of_title= []
+    list_of_message = []
+    final_list = []
+    for dictionar in data_manager.read_from_file("sample_data/question.csv"):
+        if dictionar["id"] == id:
+            list_of_title.append(dictionar["title"])
+            list_of_message.append(dictionar['message'])
+    final_list.append(list_of_title)
+    final_list.append(list_of_message)
+    return final_list
+
+
+
+def get_answer(id):
+    list_of_answers =  []
+    for dictionar in data_manager.read_from_file("sample_data/answer.csv"):
+            if dictionar["question_id"] == id:
+                list_of_answers.append(dictionar['message'])
+    return list_of_answers
+
+print(get_answer('0'))
+# print(title_and_message("1"))
