3c1c6c7ccfa9132d661d27104ddfe52761acb93f
904756@gmail.com <904756@gmail.com>
2020-02-13 10:53:19 +0200

    Add question

--- server.py
+++ server.py
@@ -11,10 +11,16 @@ def main_page():
 
 @app.route('/question/<question_id>', methods=["GET"])
 def show_questions(question_id):
-
-
     return render_template('question.html', question_elements= sorting_functions.title_and_message(question_id), answer_elements= sorting_functions.get_answer(question_id), question_id=question_id )
 
+@app.route('/add_question', methods=["GET", "POST"])
+def add_question():
+    if request.method == 'GET':
+        return render_template('add-question.html')
+    if request.method == 'POST':
+        return render_template('templates/question.html')
+    
+    
 if __name__ == '__main__':
     app.run(
         debug=True
