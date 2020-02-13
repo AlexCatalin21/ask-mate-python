eb08439b1dfb293d03464a342f98e4184c277ff0
MihaiBarbu93 <mihai.barbu1993@gmail.com>
2020-02-13 11:44:33 +0200

    add an answer

--- data_manager.py
+++ data_manager.py
@@ -1,4 +1,5 @@
 import csv
+import time
 
 
 def read_from_file(filename):
@@ -9,6 +10,24 @@ def read_from_file(filename):
             list_of_dict.append(row)
         return list_of_dict
 
+def write_to_file(filename, question_id, message):
+    with open(filename, "a+") as file:
+        HEADERS = ["id", "submission_time", "vote_number", "question_id", "message", "image"]
+        elements = csv.DictWriter(file, fieldnames=HEADERS)
+        submission_time = int(time.time())
+        vote_number = 0
+        message = message
+        image = ""
+        new_id = generate_id()
+        elements.writerow({"id": new_id,
+                           "submission_time": submission_time,
+                           "vote_number": vote_number,
+                           "question_id" : question_id,
+                           "message" : message,
+                           "image": image}
+                            )
+
+
 
 def sort_questions():
     questions = []
@@ -16,4 +35,12 @@ def sort_questions():
         "sample_data/question.csv"), key=lambda k: k["submission_time"], reverse=True)
     return sorted_list
 
+
+def generate_id():
+    list_of_dict = read_from_file("sample_data/answer.csv")
+    list_of_id = []
+    for dictionary in list_of_dict:
+        list_of_id.append(int(dictionary["id"]))
+    my_id = max(list_of_id)+ 1
+    return my_id
 
--- server.py
+++ server.py
@@ -15,6 +15,18 @@ def show_questions(question_id):
 
     return render_template('question.html', question_elements= sorting_functions.title_and_message(question_id), answer_elements= sorting_functions.get_answer(question_id), question_id=question_id )
 
+
+@app.route('/question/<question_id>/new-answer', methods=["GET", "POST"])
+def new_answers(question_id):
+    if request.method == 'POST':
+        message = request.form['message']
+        data_manager.write_to_file("sample_data/answer.csv", question_id, message)
+        return redirect(url_for('show_questions', question_id=question_id))
+    return render_template('new_answer.html', question_id=question_id)
+
+
+
+
 if __name__ == '__main__':
     app.run(
         debug=True
